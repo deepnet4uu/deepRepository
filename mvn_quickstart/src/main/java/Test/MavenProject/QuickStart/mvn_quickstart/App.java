94d00f0d59aef7e5b50cb76b68f9054cb5c770c6
Deep <deepnet4u@gmail.com>
2018-02-17 19:28:21 +0530

    "Hello World Deep!"

--- mvn_quickstart/src/main/java/Test/MavenProject/QuickStart/mvn_quickstart/App.java
+++ mvn_quickstart/src/main/java/Test/MavenProject/QuickStart/mvn_quickstart/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!321Deep" );
+        System.out.println( "Hello World Deep!" );
     }
 }
