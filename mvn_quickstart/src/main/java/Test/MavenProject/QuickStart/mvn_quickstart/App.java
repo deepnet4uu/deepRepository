bced5f5a3a3b6e22e604bbfa1bf99ba6f23e57b7
Pentest <Pentest@EH-02>
2018-02-20 16:11:25 +0530

    Deleted--- System.out.println( "abcdef!" );

--- mvn_quickstart/src/main/java/Test/MavenProject/QuickStart/mvn_quickstart/App.java
+++ mvn_quickstart/src/main/java/Test/MavenProject/QuickStart/mvn_quickstart/App.java
@@ -13,7 +13,7 @@ public class App
     	Integer C;
         System.out.println( "Hello World Deep123567!" );
         System.out.println( "Deep123567!" );
-        System.out.println( "abcdef!" );
+     
         System.out.println(C=A+B);
         System.out.println(C=A*B);
     }
