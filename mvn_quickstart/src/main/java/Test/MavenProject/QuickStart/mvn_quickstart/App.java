29871c833173ecc658d78798e96ecb4e7480c688
Pentest <Pentest@EH-02>
2018-02-20 15:23:41 +0530

    "with computation code"

--- mvn_quickstart/src/main/java/Test/MavenProject/QuickStart/mvn_quickstart/App.java
+++ mvn_quickstart/src/main/java/Test/MavenProject/QuickStart/mvn_quickstart/App.java
@@ -8,6 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
+    	Integer A=5;
+    	Integer B=4;
+    	Integer C;
         System.out.println( "Hello World Deep123567!" );
+        System.out.println(C=A+B);
+        System.out.println(C=A*B);
     }
 }
