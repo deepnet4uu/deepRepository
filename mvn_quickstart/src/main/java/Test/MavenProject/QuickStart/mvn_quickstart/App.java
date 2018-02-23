88b55766526923786f4b1b54f5f401daf42171fd
abc <abc@192.168.1.4>
2018-02-24 01:13:20 +0530

    System.out.println( "Hello_Deep123567!" );

--- mvn_quickstart/src/main/java/Test/MavenProject/QuickStart/mvn_quickstart/App.java
+++ mvn_quickstart/src/main/java/Test/MavenProject/QuickStart/mvn_quickstart/App.java
@@ -12,7 +12,7 @@ public class App
     	Integer B=4;
     	Integer C;
         System.out.println( "Hello World Deep123567!" );
-        System.out.println( "Deep123567!" );
+        System.out.println( "Hello_Deep123567!" );
      
       
      
